071f8a88ffcd5856aa214de59c24eb10acb6d005
Konstantin <trade8@bk.ru>
2022-11-20 12:49:09 +0500

    'edit-code'

--- test.py
+++ test.py
@@ -3,10 +3,6 @@ name = input('Введите имя: ')
 if name == 'Алеша':
     print('(((')
 else:
-<<<<<<< HEAD
     print('Ok. Все верно!')
-=======
 print(name)
 a = int(input('Сколько лет'))
-
->>>>>>> new-name
